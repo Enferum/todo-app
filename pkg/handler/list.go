02edce2e503c8e7ee364915b563ec637005445e2
Roman Furman <r.furman@mobidev.biz>
2021-10-08 19:00:21 +0300

    add logic to Todo List ToDo - problem with creat TodoList method

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,12 +1,31 @@
 package handler
 
 import (
+	"github.com/Enferum/todo-app"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	//fmt.Printf("%d and input %s", userId, input)
+
+	id, err := h.service.TodoList.Create(userId, input)
+	//fmt.Println("_____")
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
 	c.JSON(http.StatusOK, map[string]interface{}{
 		"id": id,
 	})
